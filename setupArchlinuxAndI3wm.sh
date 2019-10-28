43b73341011fb66d9e21e6d2520f2041146e9358
jiantian.shi <jiantian.shi@gmail.com>
2019-10-28 15:10:42 +0800

    update files

--- setupArchlinuxAndI3wm.sh
+++ setupArchlinuxAndI3wm.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 sudo pacman -Syu
+##安装工具
+sudo pacman -S core/curl extra/wget core/net-tools
 ##安装输入法
 sudo pacman -S community/fcitx-im community/fcitx-googlepinyin community/fcitx-configtool
 ##配置compton
-sudo pacman -S community/compton
-mkdir ~/.config/compton
-wget -O ~/.config/compton/config "https://raw.githubusercontent.com/shijiantian/dotfiles/master/compton/config"
+#sudo pacman -S community/compton
+#mkdir ~/.config/compton
+#wget -O ~/.config/compton/config "https://raw.githubusercontent.com/shijiantian/dotfiles/master/compton/config"
 ##配置urxvt
 sudo pacman -S community/rxvt-unicode community/wqy-zenhei
 wget -O ~/.Xresources "https://raw.githubusercontent.com/shijiantian/dotfiles/master/rxvt-unicode/Xresources"
@@ -34,13 +36,13 @@ sudo sed -i "s/^#zh_CN.UTF-8/zh_CN.UTF-8/g" /etc/locale.gen
 sudo sed -i "s/^#zh_HK.UTF-8/zh_HK.UTF-8/g" /etc/locale.gen
 sudo sed -i "s/^#zh_TW.UTF-8/zh_TW.UTF-8/g" /etc/locale.gen
 sudo locale-gen
-sudo echo "LANG=en_US.UTF-8" > /etc/locale.conf
+sudo bash -c 'echo "LANG=en_US.UTF-8" > /etc/locale.conf'
 ##设置主机名
-sudo echo "myArch" > /etc/hostname
+sudo bash -c 'echo "myArch" > /etc/hostname'
 ##设置hosts
-sudo echo "127.0.0.1    localhost" > /etc/hosts
-sudo echo "::1          localhost" >> /etc/hosts
-sudo echo "127.0.0.1    myArch.localdomain myArch" >>/etc/hosts
+sudo bash -c 'echo "127.0.0.1    localhost" >> /etc/hosts'
+sudo bash -c 'echo "::1          localhost" >> /etc/hosts'
+sudo bash -c 'echo "127.0.0.1    myArch.localdomain myArch" >>/etc/hosts'
 
 ##安装trash命令
 sudo pacman -S community/trash-cli
@@ -48,3 +50,9 @@ sudo pacman -S community/trash-cli
 wget -O ~/.bashrc "https://raw.githubusercontent.com/shijiantian/dotfiles/master/bashrc"
 source ~/.bashrc
 wget -O ~/.vimrc "https://raw.githubusercontent.com/shijiantian/dotfiles/master/vimrc"
+
+##设置lightdm
+sudo wget -O /etc/ligthdm/lightdm-gtk-greeter.conf "https://raw.githubusercontent.com/shijiantian/dotfiles/master/lightdm/lightdm-gtk-greeter.conf"
+sudo wget -O /etc/lightdm/wallpaper.jpg "https://raw.githubusercontent.com/shijiantian/dotfiles/master/wallpaper.jpg"
+
+
