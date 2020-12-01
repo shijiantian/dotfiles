cf0148697a33cb07b34df410fb5e3876c520b2a9
jiantian.shi <jiantian.shi@gmail.com>
2020-12-01 19:16:16 +0800

    修改文件

--- archInstall.sh
+++ archInstall.sh
@@ -2,14 +2,15 @@
 ## 分区
 #fdisk -l
 read -p "请输入要操作的硬盘，如sda(该操作会彻底删除硬盘上的数据，请认真核对，数据丢失概不负责):"  diskName
-#echo "d
-#n
-#p
-#1
-#
-#
-#w
-#" | fdisk /dev/$diskName && mkfs.ext4 /dev/${diskName}1
+
+echo "d
+n
+p
+1
+
+
+w
+" | fdisk /dev/$diskName && mkfs.ext4 /dev/${diskName}1
 
 #挂载分区
 echo "挂载分区..."
